ab231c63e84f26b7893603d893a419ebc5dd3f83
codeX-pratik <superpratik9861@gmail.com>
2025-04-04 23:32:00 +0530

    assignment deleted

--- SEM_6/SSWD-( Server side Web development with Node js )/ASSIGNMENT_1/routes/p4.js
+++
@@ -1,6 +0,0 @@
-import { myarea, mycircumference } from "./circle.js";
-
-const radius = 5;
-
-console.log("Area of circle with radius " + radius + " is: " + myarea(radius));
-console.log("Circumference of circle with radius " + radius + " is: " + mycircumference(radius));
