aec93d63b256e0bd39369cc81d6984f7e749ed21
codeX-pratik <superpratik9861@gmail.com>
2025-04-07 20:41:15 +0530

    oops assignment 2

--- SEM_6/OOP-( Object Oriented Programming with C++)/ASSIGNMENT_2/p2.cpp
+++ SEM_6/OOP-( Object Oriented Programming with C++)/ASSIGNMENT_2/p2.cpp
@@ -15,6 +15,7 @@ int accNumber(int accNum, BankAccount customer[]) {
 int main() {
     BankAccount customer[totalCostomer];
     for(int i=0; i<totalCostomer; i++) {
+        cout << "Enter details for customer " << i+1 << endl;
         string name, accountType;
         double balance;
         int accountNumber;
@@ -35,7 +36,10 @@ int main() {
         cout << "1. Deposit\n2. Withdraw\n3. Display\n4. Exit\n" << endl;
         cout << "Enter choice: ";
         cin >> choice;
-        if(choice == 4) { break;}
+        if(choice == 4) { 
+            cout << "Exiting..." << endl;
+            break;
+        }
         cout << "Enter account number: ";
         int accNum;
         cin >> accNum;
@@ -64,3 +68,107 @@ int main() {
     }
     return 0;
 }
+
+/*
+
+Enter details for customer 1
+Enter name: person_1
+Enter account number: 101
+Enter account type: savings
+Enter balance: 10000
+Enter details for customer 2
+Enter name: person_2
+Enter account number: 102
+Enter account type: savings
+Enter balance: 20000
+Enter details for customer 3
+Enter name: person_3
+Enter account number: 103
+Enter account type: savings
+Enter balance: 30000
+Enter details for customer 4
+Enter name: person_4
+Enter account number: 104
+Enter account type: savings
+Enter balance: 40000
+Enter details for customer 5
+Enter name: person_5
+Enter account number: 105
+Enter account type: savings
+Enter balance: 50000
+Enter details for customer 6
+Enter name: person_6
+Enter account number: 106
+Enter account type: savings
+Enter balance: 60000
+Enter details for customer 7
+Enter name: person_7
+Enter account number: 107
+Enter account type: savings
+Enter balance: 70000
+Enter details for customer 8
+Enter name: person_8
+Enter account number: 108
+Enter account type: savings
+Enter balance: 80000
+Enter details for customer 9
+Enter name: person_9
+Enter account number: 109
+Enter account type: savings
+Enter balance: 90000
+Enter details for customer 10
+Enter name: person_10
+Enter account number: 110
+Enter account type: savings
+Enter balance: 100000
+1. Deposit
+2. Withdraw
+3. Display
+4. Exit
+
+Enter choice: 3
+Enter account number: 105
+Name: person_5
+Balance: 50000
+1. Deposit
+2. Withdraw
+3. Display
+4. Exit
+
+Enter choice: 1
+Enter account number: 102
+Enter amount to deposit: 10000
+1. Deposit
+2. Withdraw
+3. Display
+4. Exit
+
+Enter choice: 3
+Enter account number: 102
+Name: person_2
+Balance: 30000
+1. Deposit
+2. Withdraw
+3. Display
+4. Exit
+
+Enter choice: 2
+Enter account number: 102
+Enter amount to withdraw: 20000
+1. Deposit
+2. Withdraw
+3. Display
+4. Exit
+
+Enter account number: 102
+Name: person_2
+Balance: 10000
+1. Deposit
+2. Withdraw
+3. Display
+4. Exit
+
+Enter choice: 4
+Exiting...
+
+*/
--- SEM_6/OOP-( Object Oriented Programming with C++)/ASSIGNMENT_2/p4.cpp
+++ SEM_6/OOP-( Object Oriented Programming with C++)/ASSIGNMENT_2/p4.cpp
@@ -1,73 +1,95 @@
 #include<iostream>
-#include<string>
 using namespace std;
 
-/*
-Create a class distance with private data members km,m,cm. Include the following member
-functions:
-input distance() with 3 parameters to initialize the distance object.
-display() to display a distance object.
-add distance() to perform addition of 2 distance objects and will return the result.
-In the main function, create an array of n distance objects, initialize and display them. Perform
-addition of any 2 distance object by prompting the user for the index of 2 objects in the array.
-Display the result.
-*/
-
 class Distance {
-    private:
-        float km, m, cm;
-    
-    public:
-        void inpute_distance(float km = 0, float m = 0, float cm = 0) {
-            this->km = km;
-            this->m = m;
-            this->cm = cm;
+private:
+    float km, m, cm;
+
+    void normalize() {
+        if (cm >= 100) {
+            m += cm / 100;
+            cm = (int)cm % 100;
         }
 
-        void display() {
-            cout << "Distance in km: " << km << " km " << endl;
-            cout << "Distance in m: " << m << " m " << endl;
-            cout << "Distance in cm: " << cm << " cm " << endl;
+        if (m >= 1000) {
+            km += m / 1000;
+            m = (int)m % 1000;
         }
+    }
 
-        Distance add_distance(Distance d) {
-            Distance result;
-            result.cm = cm + d.cm;
-            result.m = m + d.m + (result.cm / 100);
-            result.km = km + d.km + (result.m / 1000);
-            return result;
-        }
+public:
+    void inpute_distance(float k = 0, float me = 0, float c = 0) {
+        km = k;
+        m = me;
+        cm = c;
+        normalize();
+    }
+
+    void display() {
+        cout << "Distance in km: " << km << " km" << endl;
+        cout << "Distance in m: " << m << " m" << endl;
+        cout << "Distance in cm: " << cm << " cm" << endl;
+    }
+
+    Distance add_distance(Distance d) {
+        Distance result;
+        result.km = km + d.km;
+        result.m = m + d.m;
+        result.cm = cm + d.cm;
+        result.normalize();
+        return result;
+    }
 };
 
-int main () {
+int main() {
     int n;
     cout << "Enter the number of distance objects: ";
     cin >> n;
-    
+
     Distance* distances = new Distance[n];
-    
+
     for (int i = 0; i < n; i++) {
         float km, m, cm;
         cout << "Enter distance " << i + 1 << " (km m cm): ";
         cin >> km >> m >> cm;
         distances[i].inpute_distance(km, m, cm);
     }
-    
+
     for (int i = 0; i < n; i++) {
-        cout << "Distance " << i + 1 << ": ";
+        cout << "Distance " << i + 1 << ":" << endl;
         distances[i].display();
     }
-    
+
     int index1, index2;
     cout << "Enter the indices of the two distances to add (0 to " << n - 1 << "): ";
     cin >> index1 >> index2;
-    
+
     Distance result = distances[index1].add_distance(distances[index2]);
-    
-    cout << "Result of addition: " << endl;
+
+    cout << "Result of addition:" << endl;
     result.display();
-    
+
     delete[] distances;
-    
     return 0;
 }
+
+/*
+
+Enter the number of distance objects: 2
+Enter distance 1 (km m cm): 2 500 75
+Enter distance 2 (km m cm): 1 750 50
+Distance 1:
+Distance in km: 2 km
+Distance in m: 500 m
+Distance in cm: 75 cm
+Distance 2:
+Distance in km: 1 km
+Distance in m: 750 m
+Distance in cm: 50 cm
+Enter the indices of the two distances to add (0 to 1): 0 1
+Result of addition:
+Distance in km: 4.25125 km
+Distance in m: 251 m
+Distance in cm: 25 cm
+
+*/
